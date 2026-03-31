1eef146ba3a534ebb05c7894f79b37ff3ecdb3fa
unknown <doninten35@gmail.com>
2026-03-31 17:23:12 +0900

    feat: 실습문제 추가(#4)

--- ch03_recursive/main.c
+++ ch03_recursive/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
-/*void ex01(int* array, int length)
+typedef struct {
+
+	int height;
+	double weight;
+}person;
+
+void ex01(int* array, int length)
 {
-	int i;
+	int i,j;
 	int value;
 	for (i = 0; i < length; i++) 
 	{
-		value = array[i];
-		printf("%d", value);
+		 value = array[i];
+		for (j = 0;j < value; j++) {
+			printf("%d", value);
+		}
+		
 	}
-
+	printf("\n");
 }
 
 
@@ -18,13 +27,14 @@ void test_ex01()
 	int buf[3] = {5,1,4};
 	ex01(buf, 3);
 	
-}*/
+}
 
 
 void ex02(int* array, int length) {
 
 	int i;
 	int value[6];
+	
 	for (i = 0;i <length; i++) {
 		if (array[i] >= 50 && array[i] % 2 ==0)  {
 			value[i] = array[i] / 2;
@@ -33,10 +43,23 @@ void ex02(int* array, int length) {
 		else if(array[i] < 50 && array[i] % 2 !=0) {
 			value[i] = array[i] * 2;
 		}
-		printf("%d", value);
-	}
 
+		else {
+			value[i] = array[i];
+		}
+		
+	}
+	
+	
+	for (i = 0; i < length; i++) {
+		
+			printf("%d ", value[i]);    
+		}
+	
+	printf("\n");
 }
+
+
 
 void test_ex02()
 {
@@ -46,10 +69,34 @@ void test_ex02()
 
 }
 
+void ex_04(person father, person mother){
+
+	person student;
+	int i;
+
+	student.height = (int)((father.height + mother.height) / 2 + 5);
+
+	double avg_weight = (father.weight + mother.weight) / 2 - 4.5;
+	student.weight = (int)(avg_weight * 10 + 0.5) / 10.0;
+
+	printf("height: %dcm\n", student.height);
+	printf("weight %.1lfkg\n", student.weight);
+}
+void test_ex04()
+{
+	person father = { 175, 68.8 }; 
+	person mother = { 166, 56.4 }; 
+		
+	ex_04(father,mother);
+}
+ 
+
+
 int main(void) 
 {
-	//test_ex01();
+	test_ex01();
 	test_ex02();
+	test_ex04();
 
 	return 0;
 }
